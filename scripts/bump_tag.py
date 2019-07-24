57a8954374a031697d2d04d65ea2502cfa627f99
captain-kark <andrew.yurisich@nbx.com>
2019-07-24 11:06:01 +0200

    fix(bandit): Ignore subprocess shell runs

--- scripts/bump_tag.py
+++ scripts/bump_tag.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess  # nosec
 import sys
 
 if __name__ == '__main__':
     bump_level = None
     if len(sys.argv) > 1:
         bump_level = sys.argv[1]
-    latest_tag = subprocess.run(
-        ['git', 'tag', '--sort=-creatordate'],
-        check=True,
-        capture_output=True
-    ).stdout.decode().split('\n')[0]
+    latest_tag = subprocess.check_output( # nosec
+        ['/usr/bin/env', 'git', 'tag', '--sort=-creatordate']
+    ).decode().split('\n')[0]
 
     major, minor, patch = map(int, latest_tag.replace('v', '').split('.'))
     new_tag = {
